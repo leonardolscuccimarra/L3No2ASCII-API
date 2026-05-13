94320a3d4d1704de4ee467c0a449d17c844bacf4
leonardolscuccimarra <leonardolscuccimarra@gmail.com>
2026-05-13 12:28:14 -0300

    lenoServer.js ahora utiliza variables de entorno

--- lenoserver.js
+++ lenoserver.js
@@ -1,37 +1,23 @@
 import express from 'express';
-import fs from 'fs';
+import dotenv from 'dotenv';
+import {printear} from './print.js';
+
+dotenv.config();
 
 const app = express();
-const PORT = 8069;
-const IP = "0.0.0.0";
+const PORT = process.env.PORT || 8069;
+const IP = process.env.IP || "0.0.0.0";
 
 app.use(express.raw());
 
-app.get('/', (request,res)=>{
-    
-    fs.readFile('ascii-art.txt', 'utf8', (err, data) => {
-      if (err) {
-        console.error('Se rompio to:\n', err);
-        return res.status(500).send('Error reading file.');
-      }
-        res.set('Content-Type', 'text/plain');
-        res.send(data);
-    });
+app.get('/', async (request,res)=>{
+    res.send(await printear());
 });
 
-app.get('/mini', (request,res)=>{
-    
-    fs.readFile('ascii-art mini.txt', 'utf8', (err, data) => {
-      if (err) {
-        console.error('Se rompio to:\n', err);
-        return res.status(500).send('Error reading file.');
-      }
-        res.set('Content-Type', 'text/plain');
-        res.send(data);
-    });
+app.get('/mini', async (request,res)=>{
+    res.send(await printear(true));
 });
-
 
 app.listen(PORT, IP, ()=>{
-    console.log(`SERVER ON: http://${IP}:${PORT}`)
+    console.log(`SERVER ON: http://${IP == "0.0.0.0"? "localhost" : IP}:${PORT}`)
 })
